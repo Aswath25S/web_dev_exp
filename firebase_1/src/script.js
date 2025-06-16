f33ad565c673fd7064f946991094046ebd94aafe
Calvin <85937047+Aswath25S@users.noreply.github.com>
2025-06-15 22:22:31 -0400

    vite added to support firebase

---
+++ firebase_1/src/script.js
@@ -0,0 +1,93 @@
+// Imports
+import { initializeApp } from "firebase/app";
+
+// Firebase Setup
+const firebaseConfig = {
+  apiKey: "AIzaSyAVc-D6avZkFUohRwp4QFwmYt6ikM72oQI",
+  authDomain: "fir-1-44586.firebaseapp.com",
+  projectId: "fir-1-44586",
+  storageBucket: "fir-1-44586.firebasestorage.app"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log(app)
+
+// UI Elements
+const viewLoggedOut = document.getElementById("logged-out-view")
+const viewLoggedIn = document.getElementById("logged-in-view")
+
+const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
+
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("create-account-btn")
+
+const signInButtonEl = document.getElementById("sign-in-btn")
+const createAccountButtonEl = document.getElementById("create-account-btn")
+
+const signOutButtonEl = document.getElementById("sign-out-btn")
+
+const userProfilePictureEl = document.getElementById("user-profile-picture")
+const userGreetingEl = document.getElementById("user-greeting")
+
+const testButton = document.getElementById("test-btn")
+
+// UI Event Listeners
+
+
+signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
+
+signInButtonEl.addEventListener("click", authSignInWithEmail)
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
+
+testButton.addEventListener("click", onTestBtnClick)
+
+
+// Main Code 
+
+showLoggedOutView()
+
+
+// Auth Functions
+
+function authSignInWithGoogle () {
+    console.log("Sign in with Google")
+}
+
+function authSignInWithEmail () {
+    console.log("Sign in with email")
+}
+
+function authCreateAccountWithEmail () {
+    console.log("Sign in with email and password")
+}
+
+// UI Functions
+
+function showLoggedOutView() {
+    hideElement(viewLoggedIn);
+    showElement(viewLoggedOut);
+}
+
+function showLoggedInView() {
+    hideElement(viewLoggedOut);
+    showElement(viewLoggedIn);
+}
+
+function hideElement(element) {
+    element.style.display = "none";
+}
+
+function showElement(element) {
+    element.style.display = "flex";
+}
+
+function onTestBtnClick() {
+    let disp = viewLoggedOut.style.display;
+    if (disp === "none"){
+        viewLoggedOut.style.display = "flex";
+    }
+    else {
+        viewLoggedOut.style.display = "none";
+    }
+}
