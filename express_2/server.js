a89c412e033da42f4f7a8f88c5662b2429a41488
Calvin <85937047+Aswath25S@users.noreply.github.com>
2025-06-27 00:25:32 -0400

    static page added

--- express_2/server.js
+++ express_2/server.js
@@ -3,8 +3,10 @@ import express from 'express'
 const app = express();
 const port = 8000;
 
+app.use(express.static("public"))
+
 app.get('/', (req, res) => {
-    res.send(`<!doctype html><html><body><button>click</button> <br>hello calvin</body></html>`)
+    res.send(`<!doctype html><html><body>hello calvin</body></html>`)
 })
 
 app.listen(port, () => {console.log(`server started on ${port}`)})
