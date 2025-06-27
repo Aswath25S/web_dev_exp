c5793fe48c500b7914c95b9e25d41deba5d642fc
Calvin <85937047+Aswath25S@users.noreply.github.com>
2025-06-27 05:22:25 -0400

    new files added

---
+++ express_2/controllers/productsControllers.js
@@ -0,0 +1,9 @@
+export async function getGenres(req, res) {
+    console.log("get genres api called")
+    res.send("fucking god genres")
+}
+
+export async function getProducts(req, res) {
+    console.log("get products api called")
+    res.send("fucking hell and fucking god")
+}
--- express_2/server.js
+++ express_2/server.js
@@ -1,12 +1,13 @@
 import express from 'express'
+import { productsRouter } from './routes/products.js';
 
 const app = express();
 const port = 8000;
 
 app.use(express.static("public"))
+app.use('/api/products', productsRouter)
 
-app.get('/', (req, res) => {
-    res.send(`<!doctype html><html><body>hello calvin</body></html>`)
-})
-
-app.listen(port, () => {console.log(`server started on ${port}`)})
+app.listen(port, () => {console.log(`server started on ${port}`)}).
+    on('error', (error) => {
+        console.log("failed to start server : ", error)
+    })
