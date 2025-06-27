7e357c84e579fcacaa8a627fe15e8b58e8f1c3dd
Calvin <85937047+Aswath25S@users.noreply.github.com>
2025-06-26 20:21:47 -0400

    express 2 started

---
+++ express_2/server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.send(`<!doctype html><html><body><button>click</button>hello calvin</body></html>`)
+})
+
+app.listen(port, () => {console.log(`server started on ${port}`)})
