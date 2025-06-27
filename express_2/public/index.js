91187c1b4cbfd879128ad167a02a84ae462213ea
Calvin <85937047+Aswath25S@users.noreply.github.com>
2025-06-27 04:35:43 -0400

    basic js

--- express_2/public/index.js
+++ express_2/public/index.js
@@ -1,6 +1,8 @@
+// ===== Menu Toggle =====
 const toggle = document.querySelector('.menu-toggle')
 const menu = document.querySelector('.header-menu')
 
 toggle.addEventListener("click", () => {
     menu.classList.toggle("open")
 })
+
