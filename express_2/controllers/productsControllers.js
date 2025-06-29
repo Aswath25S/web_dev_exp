335bf0f4116884da94c53dea101aac3954ef139f
Calvin <85937047+Aswath25S@users.noreply.github.com>
2025-06-29 00:40:31 -0400

    database added

--- express_2/controllers/productsControllers.js
+++ express_2/controllers/productsControllers.js
@@ -1,9 +1,23 @@
-export async function getGenres(req, res, next) {
-    console.log("get genres api called")
-    next()
+import { getDBConnection } from '../db/db.js'
+
+export async function getGenres(req, res) {
+    try {
+        const db = await getDBConnection()
+        const genreRows = await db.all('SELECT DISTINCT genre FROM products')
+        const genres = genreRows.map(row => row.genre)
+        res.json(genres)
+    } catch (err) {
+        res.status(500).json({error: 'Failed to fetch genres', details: err.message})
+    }
 }
 
-export async function getProducts(req, res, next) {
-    console.log("get products api called")
-    next()
+export async function getProducts(req, res) {
+    try {
+        const db = await getDBConnection()
+        let query = 'SELECT * FROM products'
+        const products = await db.all(query)
+        res.json(products)
+    } catch (err) {
+        res.json(500).json({error: 'Failed to fetch products', details: err.message})   
+    }
 }
--- express_2/public/index.js
+++ express_2/public/index.js
@@ -1,8 +1,48 @@
-// ===== Menu Toggle =====
-const toggle = document.querySelector('.menu-toggle')
-const menu = document.querySelector('.header-menu')
+const toggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.header-menu');
 
 toggle.addEventListener("click", () => {
-    menu.classList.toggle("open")
+    menu.classList.toggle("open");
 })
 
+async function init() {
+    populateGenreSelect();
+    const products = await getProducts();
+    renderProducts(products);
+}
+
+init()
+
+async function getProducts() {
+    const res = await fetch('/api/products/');
+    return await res.json();
+}
+
+function renderProducts(products) {
+    const albumsContainer = document.getElementById('products-container');
+    const cards = products.map((album) => {
+        return `
+        <div class="product-card">
+        <img src="./images/${album.image}" alt="${album.title}">
+        <h2>${album.title}</h2>
+        <h3>${album.artist}</h3>
+        <p>$${album.price}</p>
+        <button class="add-btn">Add to Cart</button>
+        <p class="genre-label">${album.genre}</p>
+      </div>`
+    }).join('')
+    albumsContainer.innerHTML = cards
+}
+
+async function populateGenreSelect() {
+    const res = await fetch('/api/products/genres');
+    const genres = await res.json();
+    const select = document.getElementById('genre-select');
+
+    genres.forEach(genre => {
+        const option = document.createElement('option');
+        option.value = genre;
+        option.textContent = genre;
+        select.appendChild(option)
+    });
+}
