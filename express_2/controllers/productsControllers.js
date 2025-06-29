3885366a7d45dcc1570b959e78a983b413db2196
Calvin <85937047+Aswath25S@users.noreply.github.com>
2025-06-29 02:28:46 -0400

    controller done

--- express_2/controllers/productsControllers.js
+++ express_2/controllers/productsControllers.js
@@ -15,7 +15,19 @@ export async function getProducts(req, res) {
     try {
         const db = await getDBConnection()
         let query = 'SELECT * FROM products'
-        const products = await db.all(query)
+        let params = []
+        const { genre, search } = req.query
+
+        if (genre) {
+            query += ' WHERE genre = ?'
+            params.push(genre)
+        } else if (search) {
+            query += ` WHERE title LIKE ? OR artist LIKE ? OR genre LIKE ?`
+            const searchPattern = `%${search}%`
+            params.push(searchPattern, searchPattern, searchPattern)
+        }
+
+        const products = await db.all(query, params)
         res.json(products)
     } catch (err) {
         res.json(500).json({error: 'Failed to fetch products', details: err.message})   
